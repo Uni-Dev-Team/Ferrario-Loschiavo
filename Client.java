0b748c85e9474b4ffd06b1c235986679ced12815
AndreaF17 <andrea.ferrario170700@gmail.com>
2021-06-08 19:43:29 +0200

    -
    Co-authored-by: Borotalcohol <Borotalcohol@users.noreply.github.com>

--- Client.java
+++ Client.java
@@ -7,45 +7,53 @@ public class Client {
     public static ClientInfo info;
 
     public static void main(String[] args) throws Exception {
+        if (args.length != 0) {
         // Potrebbe dare errori TRY
-        try {
-            System.out.println("Started connection");
+            try {
+                System.out.println("Started connection");
 
-            // Crei il socket e gli passi l IP e la Porta
-            Socket s1 = new Socket(InetAddress.getByName("192.168.1.113"), 1234);
-            // Provi la connessione
-            System.out.println("Connected" + s1.getRemoteSocketAddress());
+                // Crei il socket e gli passi l IP e la Porta
+                Socket s1 = new Socket(InetAddress.getByName(args[0]), 1234);
+                // Provi la connessione
+                System.out.println("Connected" + s1.getRemoteSocketAddress());
 
-            // Formatti la richiesta che fai al server
-            OutputStream outToServer = s1.getOutputStream();
-            ObjectOutputStream out = new ObjectOutputStream(outToServer);
-            // DataOutputStream out = new DataOutputStream(outToServer);
-            // Invii il messaggio al server
-            // out.writeUTF("Ciao da: "+ s1.getLocalSocketAddress()+ "\n Che giorno è? ");
+                // Formatti la richiesta che fai al server
+                OutputStream outToServer = s1.getOutputStream();
+                ObjectOutputStream out = new ObjectOutputStream(outToServer);
 
-            List<Notizia.Tipo> types = new ArrayList<Notizia.Tipo>();
-            types.add(Notizia.Tipo.ATTUALITA);
-            types.add(Notizia.Tipo.POLITICA);
-            info = new ClientInfo(types, s1.getLocalSocketAddress());
-            out.writeObject(info);
 
-            while(true) {
-                InputStream inFromServer = s1.getInputStream();
-                ObjectInputStream in = new ObjectInputStream(inFromServer);
-                ServerResponse response = null;
-                try {
-                    response = (ServerResponse) in.readObject();
-                } catch(ClassNotFoundException e) {}
-                if (response != null) {
-                    System.out.println(response.toString());
+                List<Notizia.Tipo> types = new ArrayList<Notizia.Tipo>();
+                types.add(Notizia.Tipo.ATTUALITA);
+                types.add(Notizia.Tipo.POLITICA);
+                info = new ClientInfo(types, s1.getLocalSocketAddress());
+                out.writeObject(info);
+
+                InputStream inFromServer;
+                ObjectInputStream in;
+
+                while(true) {
+                    inFromServer = s1.getInputStream();
+                    in = new ObjectInputStream(inFromServer);
+                    ServerResponse response = null;
+                    try {
+                        response = (ServerResponse) in.readObject();
+                    } catch(ClassNotFoundException e) {}
+                    if (response != null) {
+                        System.out.println(response.toString());
+                    }
                 }
-            }
 
-            // Chiudi la connessione
-            // s1.close();
-        }
-        catch(IOException e) {
-            e.printStackTrace();
+                // inFromServer.close();
+                // in.close();
+
+                // Chiudi la connessione
+                // s1.close();
+            }
+            catch(IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("No ip specified");
         }
     }
 }
